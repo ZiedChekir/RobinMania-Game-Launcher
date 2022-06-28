e7b4074ccc2c256155de99feddcd118a7228253d
zied <ziedchekir1@gmail.com>
2022-06-28 19:36:32 +0100

    Updates Nfts + hashing xxhash algorithm + icon

---
+++ GameLauncher/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher
+{
+    class Constants
+    {
+        public static readonly string GAME_TITLE = "RobinMania";
+        public static readonly string GAME_LAUNCHER = "RobinMania Launcher";
+
+        //API 
+        public static readonly string VERSION_DOWNLOAD_URI = "http://localhost:3000/download/version";
+        public static readonly string GAME_DOWNLOAD_URI = "http://localhost:3000/download/game";
+        public static readonly string NFTJSON_DOWNLOAD_URI = "http://localhost:3000/download/nftjson";
+        public static readonly string NFTS_DOWNLOAD_URI = "http://localhost:3000/download/nfts";
+        public static readonly string NFTS_DOWNLOAD_CUSTOM_URI = "http://localhost:3000/download/nfts/custom";
+
+    }
+}
--- GameLauncher/MainWindow.axaml.cs
+++ GameLauncher/MainWindow.axaml.cs
@@ -1,28 +1,42 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using Standart.Hash.xxHash;
 
 namespace GameLauncher
 {
     public partial class MainWindow : Window
     {
+
+        private bool rendered = false;
+
         public enum LauncherStatus
         {
             ready,
             downloading_game,
             downloading_updates,
+            downloading_nfts,
             failed,
         }
+
         private string rootPath;
         private string versionFile;
         private string gameZip;
+        private string gameDirectory;
         private string gameExe;
-        private string nftJson;
+        private string nftJsonFile;
+        private string nftZip;
         private string nftDirectory;
 
         private LauncherStatus _status;
@@ -46,63 +60,55 @@ namespace GameLauncher
                     case LauncherStatus.downloading_updates:
                         PlayButton.Content = "Downloading Update";
                         break;
+                    case LauncherStatus.downloading_nfts:
+                        PlayButton.Content = "Downloading NFTs";
+                        break;
                     default:
                         break;
                 }
             }
-            
+
         }
 
-        
+
         public MainWindow()
         {
             InitializeComponent();
-
-            
+            rendered = false;
             rootPath = Directory.GetCurrentDirectory();
             versionFile = Path.Combine(rootPath, "Version.txt");
-            gameZip = Path.Combine(rootPath, "Build.zip");
-            gameExe = Path.Combine(rootPath, "Build", "Pirate Game.exe");
-            nftJson = Path.Combine(rootPath, "nfts.json");
+            gameZip = Path.Combine(rootPath, "game.zip");
+            gameExe = Path.Combine(rootPath, "game", "networking.exe");
+            nftJsonFile = Path.Combine(rootPath, "Nfts.json");
             nftDirectory = Path.Combine(rootPath, "nfts");
-
-            //hashtest();
-
-            //CheckForUpdates();
+            gameDirectory = Path.Combine(rootPath, "game");
+            nftZip = Path.Combine(rootPath, "Nfts.zip");
         }
 
-        protected override void OnInitialized()
+
+        private void MainWindow_Activated(object? sender, EventArgs e)
         {
-            base.OnInitialized();
-            hashtest();
+            if (rendered)
+                return;
+            CheckForUpdates();
+            rendered = true;
         }
-        private void hashtest()
+
+        private uint hash(byte[] data,  uint seed = 0)
         {
-
-            string p = Path.Combine(rootPath, "logonew.png");
-            uint x = 0;
-            for (int i = 0; i < 100000; i++)
-            {
-                 x = MurmurHash3_x86_32(File.ReadAllBytes(p), 10, 2);
-
-            }
-
-            var messageBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error", x.ToString()) ;
-            messageBoxStandardWindow.Show();
-
+            return xxHash32.ComputeHash(data, data.Length, seed);
         }
+
         private void CheckForUpdates()
         {
-            if (File.Exists(versionFile))
+            if (File.Exists(versionFile) && Directory.Exists(gameDirectory))
             {
-                
                 Version localVersion = new Version(File.ReadAllText(versionFile));
                 VersionText.Text = localVersion.ToString();
-
                 try
                 {
                     WebClient webClient = new WebClient();
-                    Version onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1R3GT_VINzmNoXKtvnvuJw6C86-k3Jr5s"));
+                    Version onlineVersion = new Version(webClient.DownloadString(Constants.VERSION_DOWNLOAD_URI));
 
                     if (onlineVersion.IsDifferentThan(localVersion))
                     {
@@ -110,7 +116,7 @@ namespace GameLauncher
                     }
                     else
                     {
-                        Status = LauncherStatus.ready;
+                        CheckForNftsUpdate();
                     }
                 }
                 catch (Exception ex)
@@ -139,11 +145,11 @@ namespace GameLauncher
                 else
                 {
                     Status = LauncherStatus.downloading_game;
-                    _onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1R3GT_VINzmNoXKtvnvuJw6C86-k3Jr5s"));
+                    _onlineVersion = new Version(webClient.DownloadString(Constants.VERSION_DOWNLOAD_URI));
                 }
 
                 webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
-                webClient.DownloadFileAsync(new Uri("https://drive.google.com/uc?export=download&id=1SNA_3P5wVp4tZi5NKhiGAAD6q4ilbaaf"), gameZip, _onlineVersion);
+                webClient.DownloadFileAsync(new Uri(Constants.GAME_DOWNLOAD_URI), gameZip, _onlineVersion);
             }
             catch (Exception ex)
             {
@@ -158,15 +164,12 @@ namespace GameLauncher
             try
             {
                 string onlineVersion = ((Version)e.UserState).ToString();
-                ZipFile.ExtractToDirectory(gameZip, rootPath, true);
+                ZipFile.ExtractToDirectory(gameZip, gameDirectory, true);
                 File.Delete(gameZip);
-
                 File.WriteAllText(versionFile, onlineVersion);
-
                 VersionText.Text = onlineVersion;
+                CheckForNftsUpdate();
 
-
-                Status = LauncherStatus.ready;
             }
             catch (Exception ex)
             {
@@ -177,28 +180,105 @@ namespace GameLauncher
         }
         private void CheckForNftsUpdate()
         {
-            if (File.Exists(nftJson))
+            try
             {
-                //download the nftjson from server 
-                //compare it to this one ( hash every nft and compare it to the downloaded one ) 
-                //get list of nfts to download 
-                //send them to server 
-                //server zips them and sends them  
-                //launcher download the nfts 
-                //unzip in nftDirectory
-                //ready to play
+
+
+                WebClient webClient = new WebClient();
+                string onlineNftJsonData = webClient.DownloadString(Constants.NFTJSON_DOWNLOAD_URI);
+
+                NftsJson onlineNftVersion = new NftsJson(onlineNftJsonData);
+                if (Directory.Exists(nftDirectory))
+                {
+                    foreach (string nftFileName in onlineNftVersion.NFTJSON.Keys)
+                    {
+                        string nftFileNamePath = Path.Combine(nftDirectory, nftFileName);
+                        if (File.Exists(nftFileNamePath))
+                        {
+                            uint LocalFileHash = hash(File.ReadAllBytes(nftFileNamePath));
+                            onlineNftVersion.CheckHash(nftFileName, LocalFileHash.ToString());
+                        }
+                        else
+                        {
+                            onlineNftVersion.AddMissingNft(nftFileName);
+                        }
+                    }
+                    if (onlineNftVersion.MissingNfts.Count > 0)
+                    {
+
+                        string json = JsonConvert.SerializeObject(onlineNftVersion.MissingNfts, Formatting.None);
+                        DownloadNfts(false, json);
+
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                    }
+                }
+                else
+                {
+                    File.WriteAllText(nftJsonFile, onlineNftJsonData);
+                    DownloadNfts(true);
+                }
+            }catch(Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                var messageBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error", $"Error NFT update: {ex}");
+                messageBoxStandardWindow.Show();
             }
-            else
+
+
+        }
+
+        private void DownloadNfts(bool donwloadAllNfts, string JsonNftToDownload = null)
+        {
+            try
             {
-                //download all the nfts 
-                //unzip them
-                //ready to play 
-                DownloadNfts(true);
+
+
+                Status = LauncherStatus.downloading_nfts;
+                WebClient webClient = new WebClient();
+                if (donwloadAllNfts)
+                {
+                    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadNftsCompletedCallback);
+                    webClient.DownloadFileAsync(new Uri(Constants.NFTS_DOWNLOAD_URI), nftZip, "123");
+                }
+                else
+                {
+                    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadNftsCompletedCallback);
+                    webClient.DownloadFileAsync(new Uri(Constants.NFTS_DOWNLOAD_CUSTOM_URI + "/?nfts=" + JsonNftToDownload), nftZip, "1234");
+                }
+            }catch(Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                var messageBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error", $"Error NFT download: {ex}");
+                messageBoxStandardWindow.Show();
             }
         }
-        private void DownloadNfts(bool donwloadAllNfts)
-        {
 
+        //private void posthttp()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    Debug.WriteLine(JsonNftToDownload);
+        //    StringContent content = new StringContent(JsonNftToDownload, Encoding.UTF8, "application/json");
+
+        //    Debug.WriteLine(content);
+        //    var response = client.PostAsync(Constants.NFTS_DOWNLOAD_CUSTOM_URI, content).ConfigureAwait(false);
+        //}
+        private void DownloadNftsCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(nftZip, nftDirectory, true);
+                File.Delete(nftZip);
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                var messageBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error", $"Error  downloading NFTS: {ex}");
+                messageBoxStandardWindow.Show();
+            }
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
@@ -206,7 +286,7 @@ namespace GameLauncher
             if (File.Exists(gameExe) && Status == LauncherStatus.ready)
             {
                 ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
-                startInfo.WorkingDirectory = Path.Combine(rootPath, "Build");
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "game");
                 Process.Start(startInfo);
 
                 Close();
@@ -216,144 +296,97 @@ namespace GameLauncher
                 CheckForUpdates();
             }
         }
-        static public uint MurmurHash3_x86_32(byte[] data, uint length, uint seed)
-        {
-            uint nblocks = length >> 2;
-
-            uint h1 = seed;
-
-            const uint c1 = 0xcc9e2d51;
-            const uint c2 = 0x1b873593;
-
-            //----------
-            // body
-
-            int i = 0;
-
-            for (uint j = nblocks; j > 0; --j)
-            {
-                uint k1l = BitConverter.ToUInt32(data, i);
-
-                k1l *= c1;
-                k1l = rotl32(k1l, 15);
-                k1l *= c2;
-
-                h1 ^= k1l;
-                h1 = rotl32(h1, 13);
-                h1 = h1 * 5 + 0xe6546b64;
-
-                i += 4;
-            }
-
-            //----------
-            // tail
-
-            nblocks <<= 2;
-
-            uint k1 = 0;
-
-            uint tailLength = length & 3;
-
-            if (tailLength == 3)
-                k1 ^= (uint)data[2 + nblocks] << 16;
-            if (tailLength >= 2)
-                k1 ^= (uint)data[1 + nblocks] << 8;
-            if (tailLength >= 1)
-            {
-                k1 ^= data[nblocks];
-                k1 *= c1; k1 = rotl32(k1, 15); k1 *= c2; h1 ^= k1;
-            }
-
-            //----------
-            // finalization
-
-            h1 ^= length;
-
-            h1 = fmix32(h1);
-
-            return h1;
-        }
-        static uint fmix32(uint h)
-        {
-            h ^= h >> 16;
-            h *= 0x85ebca6b;
-            h ^= h >> 13;
-            h *= 0xc2b2ae35;
-            h ^= h >> 16;
-
-            return h;
-        }
-
-        static uint rotl32(uint x, byte r)
-        {
-            return (x << r) | (x >> (32 - r));
-        }
     }
     struct NftsJson
     {
-        //dictionary 
-        //compare name and hash function
-        //constructor create nftsJson from a string of Json data
-        //Json data nfts name => hash
-        //nfts not found list to send to the server 
-      
-    }
-        struct Version
+        private Dictionary<string, string> nftjson;
+        private List<string> missingNfts;
+
+        public Dictionary<string, string> NFTJSON => nftjson;
+        public List<string> MissingNfts => missingNfts;
+        internal NftsJson(string data)
         {
-            internal static Version zero = new Version(0, 0, 0);
+            nftjson = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
+            missingNfts = new List<string>();
+        }
 
-            private short major;
-            private short minor;
-            private short subMinor;
 
-            internal Version(short _major, short _minor, short _subMinor)
+
+        internal bool HashCorrect(string key, string hash)
+        {
+            return nftjson[key] == hash;
+        }
+        internal void AddMissingNft(string missingNft)
+        {
+            missingNfts.Add(missingNft);
+        }
+
+        internal void CheckHash(string key, string hash)
+        {
+            if (HashCorrect(key, hash))
+                return;
+            else
+                AddMissingNft(key);
+        }
+
+
+    }
+    struct Version
+    {
+        internal static Version zero = new Version(0, 0, 0);
+
+        private short major;
+        private short minor;
+        private short subMinor;
+
+        internal Version(short _major, short _minor, short _subMinor)
+        {
+            major = _major;
+            minor = _minor;
+            subMinor = _subMinor;
+        }
+        internal Version(string _version)
+        {
+            string[] versionStrings = _version.Split('.');
+            if (versionStrings.Length != 3)
             {
-                major = _major;
-                minor = _minor;
-                subMinor = _subMinor;
+                major = 0;
+                minor = 0;
+                subMinor = 0;
+                return;
             }
-            internal Version(string _version)
-            {
-                string[] versionStrings = _version.Split('.');
-                if (versionStrings.Length != 3)
-                {
-                    major = 0;
-                    minor = 0;
-                    subMinor = 0;
-                    return;
-                }
 
-                major = short.Parse(versionStrings[0]);
-                minor = short.Parse(versionStrings[1]);
-                subMinor = short.Parse(versionStrings[2]);
+            major = short.Parse(versionStrings[0]);
+            minor = short.Parse(versionStrings[1]);
+            subMinor = short.Parse(versionStrings[2]);
+        }
+
+        internal bool IsDifferentThan(Version _otherVersion)
+        {
+            if (major != _otherVersion.major)
+            {
+                return true;
             }
-
-            internal bool IsDifferentThan(Version _otherVersion)
+            else
             {
-                if (major != _otherVersion.major)
+                if (minor != _otherVersion.minor)
                 {
                     return true;
                 }
                 else
                 {
-                    if (minor != _otherVersion.minor)
+                    if (subMinor != _otherVersion.subMinor)
                     {
                         return true;
                     }
-                    else
-                    {
-                        if (subMinor != _otherVersion.subMinor)
-                        {
-                            return true;
-                        }
-                    }
                 }
-                return false;
             }
+            return false;
+        }
 
-            public override string ToString()
-            {
-                return $"{major}.{minor}.{subMinor}";
-            }
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{subMinor}";
         }
     }
+}
